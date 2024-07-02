59b1f716853190c011e44658a5203338ddd2a29d
choimisun111 <chlaltjs111@naver.com>
2024-07-02 16:22:59 +0900

    python활용 데이터 분석
    python활용 데이터 분석

---
+++ list.py
@@ -0,0 +1,25 @@
+misun= ["최미선", '32', '010-3456-****']
+name = misun[0]
+age = misun[1]
+phone = misun[2]
+print(type(misun))
+print(name, age, phone)
+
+names =[['강수경', '강혜나', '김민석'],['20','21','22',],['010-****-****','010-****-****','010-****-****']]
+print(names[0][0:2])
+
+numbers = [1,2,3,4,5]
+result = numbers[2] + numbers[-1]
+print(result)
+
+print(len(names[0]))
+
+
+# 리스트에 요소 추가하기
+last = [1,2,3]
+last.append(30)
+print(last)
+last.remove(3)
+print(last)
+print(type(last))
+
